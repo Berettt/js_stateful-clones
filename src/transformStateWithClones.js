af55105d4febc8d397b5a1c21dd0cfd9293ea503
Michał Filip <biznesmf@gmail.com>
2026-02-07 21:23:15 +0100

    First commit/ tests ok

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const FINAL_STATES = [];
+  let newState = Object.assign({}, state);
+  const ADD_ACTION = 'addProperties';
+  const REMOVE_ACTION = 'removeProperties';
+  const CLEAR_ALL_ACTIONS = 'clear';
 
+  for (const action of actions) {
+    if (action.type === ADD_ACTION) {
+      newState = Object.assign({}, newState, action.extraData);
+    }
+
+    if (action.type === REMOVE_ACTION) {
+      newState = { ...newState };
+
+      for (const deleteKey of action.keysToRemove) {
+        delete newState[deleteKey];
+      }
+    }
+
+    if (action.type === CLEAR_ALL_ACTIONS) {
+      newState = {};
+    }
+
+    FINAL_STATES.push(newState);
+  }
+
+  return FINAL_STATES;
+}
 module.exports = transformStateWithClones;
