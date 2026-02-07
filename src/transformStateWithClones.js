edc0e1f3ab371d3e5a75ab63ef718fe870bedfc6
Michał Filip <biznesmf@gmail.com>
2026-02-07 21:34:56 +0100

    added default to svitch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -27,6 +27,8 @@ function transformStateWithClones(state, actions) {
       case 'clear':
         newState = {};
         break;
+      default:
+        break;
     }
 
     FINAL_STATES.push(newState);
